c3043cb2c5ca2a7ea88523aca207eed100a17328
John  Tucker <john@larkintuckerllc.com>
2017-11-19 11:41:58 -0500

    Bootstrap

--- functions.php
+++ functions.php
@@ -1,11 +1,41 @@
 <?php
 function mytheme_add_theme_scripts() {
+  wp_deregister_script( 'jquery' );
+  wp_register_style( 'bootstrap',
+    get_template_directory_uri() .
+    '/node_modules/bootstrap/dist/css/bootstrap.min.css',
+    array(), '2017111901', 'all' );
   wp_register_style( 'style', get_stylesheet_uri(),
     array(), '2017111801', 'all' );
+  wp_register_script( 'bootstrap',
+    get_template_directory_uri() .
+    '/node_modules/bootstrap/dist/js/bootstrap.min.js',
+    array( 'jquery', 'popper' ), '2017111901', true );
+  wp_register_script( 'jquery',
+    get_template_directory_uri() .
+    '/node_modules/jquery/dist/jquery.min.js',
+    array(), '2017111901', true );
+  wp_register_script( 'popper',
+    get_template_directory_uri() .
+    '/node_modules/popper.js/dist/umd/popper.min.js',
+    array(), '2017111901', true );
   wp_enqueue_style( 'style' );
+  wp_enqueue_style( 'bootstrap' );
+  wp_enqueue_script( 'bootstrap' );
+  wp_enqueue_script( 'jquery' );
+  wp_enqueue_script( 'popper' );
 }
 function mytheme_register_my_menus() {
   register_nav_menu( 'header', __( 'Header' ) );
 }
+function mytheme_nav_menu_css_class($classes, $item, $args) {
+  $classes[] = 'nav-item';
+  return $classes;
+}
+function mytheme_wp_nav_menu($ulclass) {
+  return preg_replace( '/<a /', '<a class="nav-link"', $ulclass );
+}
 add_action( 'init', 'mytheme_register_my_menus' );
 add_action( 'wp_enqueue_scripts', 'mytheme_add_theme_scripts' );
+add_filter( 'nav_menu_css_class', 'mytheme_nav_menu_css_class', 10, 3 );
+add_filter( 'wp_nav_menu', 'mytheme_wp_nav_menu' );
--- index.php
+++ index.php
@@ -7,10 +7,21 @@
     <?php wp_head(); ?>
   </head>
   <body>
-    <?php wp_nav_menu( array( 'theme_location' => 'header' ) ); ?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <?php wp_nav_menu( array(
+          'theme_location' => 'header',
+          'menu_class' => 'navbar-nav mr-auto'
+        ) ); ?>
+      </div>
+    </nav>
     <?php while ( have_posts() ) : the_post(); ?>
-      <div>
-        <div><?php the_title(); ?><div>
+      <div class="container">
+        <h3><?php the_title(); ?></h3>
         <?php the_content(); ?>
       </div>
     <?php endwhile; ?>
