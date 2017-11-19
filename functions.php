64d780260ed3414911c3d889763e41fcbdfebc6b
John  Tucker <john@larkintuckerllc.com>
2017-11-19 07:41:43 -0500

    Menus

--- functions.php
+++ functions.php
@@ -1,7 +1,11 @@
 <?php
 function mytheme_add_theme_scripts() {
-  wp_register_style('style', get_stylesheet_uri(),
-    array(), '2017111801', 'all');
-  wp_enqueue_style('style');
+  wp_register_style( 'style', get_stylesheet_uri(),
+    array(), '2017111801', 'all' );
+  wp_enqueue_style( 'style' );
 }
-add_action('wp_enqueue_scripts', 'mytheme_add_theme_scripts');
+function mytheme_register_my_menus() {
+  register_nav_menu( 'header', __( 'Header' ) );
+}
+add_action( 'init', 'mytheme_register_my_menus' );
+add_action( 'wp_enqueue_scripts', 'mytheme_add_theme_scripts' );
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <?php wp_head(); ?>
   </head>
   <body>
+    <?php wp_nav_menu( array( 'theme_location' => 'header' ) ); ?>
     <?php while ( have_posts() ) : the_post(); ?>
       <div>
         <div><?php the_title(); ?><div>
